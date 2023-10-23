23bbe05fef327f234ec6d31a069ac3928a84d2d8
Muhammad Saboor Islam <97667489+MuhammadSaboorIslam@users.noreply.github.com>
2023-10-23 16:02:07 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 
-import App from "./App.js";
-
-// OMDb Api Key:  4280eb5b
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
